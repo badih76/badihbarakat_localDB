e99be29d37cc5030637099ee73dea25bfdf33cdb
Badih I Barakat <badih.barakat@badihbarakat.info>
2023-05-12 14:17:33 +1000

    bodyStyle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         <link rel='stylesheet' href='./globals.css' />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body style={{backgroundColor: '#008080'}}>
+      <body className='bodyStyle'>
         <header>
           <BSNavBar />  
         </header>
